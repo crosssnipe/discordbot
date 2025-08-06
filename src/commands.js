257b35b79422a14cde9f6f2d8fd9538d5ad92a6b
Ian Lee <ianlee11103@gmail.com>
2025-08-06 16:16:37 -0700

    working code

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+export const SNIPE_COMMAND = {
+    name: 'snipe',
+    description: 'Log a snipe for points! Up to five targets in one command.',
+};
+
+export const PING_COMMAND = {
+    name: 'ping',
+    description: 'Ping the bot to check if it is online.',
+};
